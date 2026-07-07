e95ba2d3eeb34ef571378435bf3d124da2733a46
Anil kumar <anilkumarkilli29@gmail.com>
2026-07-07 22:35:27 +0530

    add transaction withdraw

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -47,4 +47,41 @@ exports.deposit = async (req, res) => {
 exports.withdraw = async (req, res) => {
     const session = await mongoose.startSession();
     session.startTransaction();
+    try{
+        const { accountId, amount } = req.body;
+
+        if (amount <= 0) return res.status(400).json({ msg: 'Amount must be greater than 0' });
+
+        const account = await Account.findOne({ _id: accountId, user: req.user.id }).session(session);
+        if (!account) {
+            await session.abortTransaction();
+            return res.status(404).json({ msg: 'Account not found or unauthorized' });
+        }
+
+        if (account.status !== 'active') {
+            await session.abortTransaction();
+            return res.status(400).json({ msg: 'Account is blocked and cannot perform transactions' });
+         }
+
+        account.balance -= amount;
+        await account.save({ session });
+
+        const transaction = new Transaction({
+            toAccount: account._id,
+            amount,
+            type: 'withdraw'
+        });
+        await transaction.save({ session });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.json({ msg: 'Withdrawal successful', balance: account.balance, transaction });
+    } catch (err) {
+        await session.abortTransaction();
+        console.error('Error occurred while processing withdrawal:', err);
+        res.status(500).json({ msg: 'Internal server error' });
+    } finally {
+        await session.endSession();
+    }
 }
